dcde28a6e43aefaedd6901e516a6097c9fd59df0
hornedratt <kuzja.i01@mail.ru>
2023-09-24 23:18:01 +0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='reduction of dimentions in bacil MS profiles trough autoencoders and following classification  ',
+    author='Irina Kuzmicheva',
+    license='MIT',
+)
